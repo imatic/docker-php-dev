3ca467afe9ecd52c8eb9307fba9a5c633b57c731
Stepan Koci <stepan.koci@imatic.cz>
2016-11-21 00:37:27 +0100

    Added PHP extensions.

--- versions/7.0-apache/install.sh
+++ versions/7.0-apache/install.sh
@@ -6,11 +6,14 @@ apt-get update -y
 apt-get install -y \
         libapache2-mod-php7.0 \
         php7.0-curl \
+        php7.0-ctype \
         php7.0-gd \
         php7.0-intl \
         php7.0-json \
+        php7.0-mbstring \
         php7.0-mcrypt \
         php7.0-mysql \
-        php7.0-pgsql
+        php7.0-pgsql \
+        php7.0-xml
 
 apt-get clean
